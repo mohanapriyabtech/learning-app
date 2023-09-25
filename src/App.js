491c058d8303ab11acebf2d77a02879ecbdb4bf7
mohanapriyabtech <mohana.priya@sparkouttech.com>
2023-09-26 00:32:25 +0530

    signup and login integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Login from "./pages/component/Login";
+import Register from "./pages/component/Register";
+import ForgotPassword from "./pages/component/ForgotPassword";
+import Project from "./pages/component/Project";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Login />}></Route>
+        <Route path="/register" exact element={<Register />}></Route>
+        <Route path="/reset-password" exact element={<ForgotPassword />}></Route>
+        <Route path="/project" exact element={<Project />}></Route>
+      </Routes>
+    </BrowserRouter>
+   </>
+  )
 }
-
-export default App;
